7934960c9b1f3a1bb914cb044acd30801bb55305
Muhamad Irham Prasetyo <muhamad.prasetyo@enerxess.com>
2019-04-10 21:05:11 +0200

    final code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Converter from "number-to-words";
+import {Button, Form, FormGroup, Label, Input, Container} from 'reactstrap';
+import Clipboard from 'clipboard';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		converter: ''
+	};
+	
+	componentDidMount() {
+		this.clipboard = new Clipboard('.converter');
+		this.clipboard.on('success', () => {
+			console.log('Copied')
+		})
+	}
+	
+	onSubmit = (e) => {
+		e.preventDefault();
+		let value = e.target.elements.convert.value;
+		return this.setState({converter: Converter.toWords(value)});
+	};
+	
+	render() {
+		const { converter } = this.state;
+		return (
+			<Container>
+				<h1>Number to words converter</h1>
+				<Form onSubmit={this.onSubmit}>
+					<FormGroup>
+						<Label>Value
+							<Input type="number" name="convert" defaultValue={'0'}/>
+						</Label>
+					</FormGroup>
+					<Button color="danger">Convert to words</Button>
+				</Form>
+				
+				{converter &&
+				<div>
+					<h3>{converter}</h3>
+					<Button
+						data-clipboard-text={converter}
+						data-clipboard-action="copy"
+						className="converter"
+					>
+						Copy
+					</Button>
+				</div>}
+			</Container>
+		);
+	}
 }
 
 export default App;
